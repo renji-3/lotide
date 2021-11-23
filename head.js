36cf0f6958ff59b92d0b1d8b1b37e5b92b235cf0
Ren Guidolin <8.renji@gmail.com>
2021-11-22 19:15:49 -0500

    head

--- head.js
+++ head.js
@@ -9,6 +9,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+let head = function(array) {
+  return array[0];
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
