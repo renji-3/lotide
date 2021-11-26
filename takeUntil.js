5b0f5ff57776d0e1779919c1f5867030f98cb918
Ren Guidolin <8.renji@gmail.com>
2021-11-25 20:34:34 -0500

    takeuntil2

--- takeUntil.js
+++ takeUntil.js
@@ -25,10 +25,10 @@ let assertArraysEqual = function(arr, arr2) {
 const takeUntil = function(array, callback) {
   const endArr = [];
   for (let item of array) {
-    if (callback(item)) {
+    if (callback(item)) { //if the callback item happens, stop the function, dont even add it to the array
       return endArr;
     } else {
-      endArr.push(item);
+      endArr.push(item); //the callback function doesnt happen until later in the array, so it loops through the array, sees the condition isnt met, pushes it into the new array
     }
   }
   return endArr;
