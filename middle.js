2ceab63942e5844ba9b810590aba53e33f97b9d3
Ren Guidolin <8.renji@gmail.com>
2021-11-29 17:14:29 -0500

    mid

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-let eqArrays = function(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < b.length; j++) {
-      if (a[i] !== b[j]) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  }
-};
-
-let assertArraysEqual = function(arr, arr2) {
-  if (eqArrays(arr, arr2) === false) {
-    console.log('shits fugaze');
-  } else {
-    console.log('shits legit');
-  }
-};
-
-
 const middle = function(arr) {
   let newA = [];
   if (arr.length > 2) {
@@ -35,14 +11,8 @@ const middle = function(arr) {
   return newA;
 };
 
+module.exports = middle;
 
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([1, 2]), [3]); // => []
 
 
-/*assertArraysEqual(middle([1, 2, 3, 4])); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6])); // => [3, 4])
-assertArraysEqual(middle([1])); // => []
-assertArraysEqual(middle([1, 2])); // => []*/
